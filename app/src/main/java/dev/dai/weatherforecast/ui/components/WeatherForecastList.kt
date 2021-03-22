d850f890b854157755dfde4a3a072496dbf363bf
Dai1678 <dai7774@gmail.com>
2021-03-22 15:31:40 +0900

    create String.toWeatherIconUrl

--- app/src/main/java/dev/dai/weatherforecast/ui/components/WeatherForecastList.kt
+++ app/src/main/java/dev/dai/weatherforecast/ui/components/WeatherForecastList.kt
@@ -16,12 +16,7 @@
 package dev.dai.weatherforecast.ui.components
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
@@ -36,6 +31,7 @@ import androidx.compose.ui.unit.dp
 import com.google.accompanist.coil.CoilImage
 import dev.dai.weatherforecast.model.WeatherForecast
 import dev.dai.weatherforecast.model.weatherForecastItems
+import dev.dai.weatherforecast.util.toWeatherIconUrl
 
 @Composable
 fun WeatherForecastList(modifier: Modifier = Modifier) {
@@ -59,9 +55,10 @@ private fun WeatherForecastItem(weatherForecast: WeatherForecast) {
         verticalAlignment = Alignment.CenterVertically
     ) {
         CoilImage(
-            data = weatherForecast.getWeatherIconUrl(),
+            data = weatherForecast.weatherIconId.toWeatherIconUrl(),
             contentDescription = "weather icon",
-            fadeIn = true
+            fadeIn = true,
+            modifier = Modifier.size(32.dp)
         )
         Spacer(modifier = Modifier.width(8.dp))
         Text(
