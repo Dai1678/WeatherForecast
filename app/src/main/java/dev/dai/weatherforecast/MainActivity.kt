1fb62c0e93381e9525effb1997142cefac5cdacc
Dai1678 <dai7774@gmail.com>
2021-03-22 23:27:27 +0900

    execute spotlessApply

--- app/src/main/java/dev/dai/weatherforecast/MainActivity.kt
+++ app/src/main/java/dev/dai/weatherforecast/MainActivity.kt
@@ -19,7 +19,6 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
